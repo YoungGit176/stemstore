e538770db3d74510332864d263a92cdf5797d2e1
qurui <quruis7@gmail.com>
2021-07-03 19:28:09 +0800

    init

---
+++ src/test/java/com/example/stemstore/StemstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stemstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

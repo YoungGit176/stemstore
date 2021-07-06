6922527b955f9cf34b5627196bb9e54e8de0a651
catiy <1815780801@qq.com>
2021-07-06 21:29:57 +0800

    新增getusername方法

--- src/main/java/com/example/stemstore/bean/User.java
+++ src/main/java/com/example/stemstore/bean/User.java
@@ -7,24 +7,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
+
 @Data
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
     private int userId;
     private String userName;
     private int user_pwd;
     @TableField(exist = false)
     private int logic;
 
-//    public int getid(){
-//        return userId;
-//    }
-//    public int getpwd(){
-//        return user_pwd;
-//    }
-//    public String getUserName(){
-//        return userName;
-//    }
+    public int getid(){
+        return userId;
+    }
+    public int getpwd(){
+        return user_pwd;
+    }
+    public String getUserName(){
+        return userName;
+    }
 }
